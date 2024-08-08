c36f4c2c9d4ceaea05e7c8fd41571aa9a169cfb8
Solractys <135873875+Solractys@users.noreply.github.com>
2024-08-08 17:21:04 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import routes from "./routes"
+import routes from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 
