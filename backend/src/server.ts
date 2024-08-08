a347ff5eaeb4c15a691a5f8e7d25f91eab9f8311
Solractys <135873875+Solractys@users.noreply.github.com>
2024-08-08 16:24:04 -0300

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import app from "./app";
 
+import cors from "cors";
+
 const port = process.env.PORT;
 
+app.use(
+  cors({ origin: "*", methods: "GET, POST, PUT, DELETE", credentials: true })
+);
 app.listen(port, () => {
   console.log(`Server rodando em http://localhost:${port}`);
-  }
-)
+});
