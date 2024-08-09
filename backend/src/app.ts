5b0dab0c0a3969e927fc37b570990893d03bc6d7
Solractys <135873875+Solractys@users.noreply.github.com>
2024-08-09 19:12:41 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,14 +7,15 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
+    allowedHeaders: "Content-Type, Authorization",
   })
 );
 
 app.use(bodyParser.json());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 export default app;
