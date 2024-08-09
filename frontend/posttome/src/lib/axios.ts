29c9fc44ca80b7088f7abc05e332fb15b288bc7f
Solractys <135873875+Solractys@users.noreply.github.com>
2024-08-09 19:57:48 -0300

    Update axios.ts

--- frontend/posttome/src/lib/axios.ts
+++ frontend/posttome/src/lib/axios.ts
@@ -3,4 +3,9 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://post-to-me-production.up.railway.app/api",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type",
+  },
 });
